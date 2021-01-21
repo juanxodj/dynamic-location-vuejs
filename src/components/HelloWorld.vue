bc855f690a6c865ad4e1eff43933317ab1e11153
Juan Sánchez <jlsc92@outlook.es>
2021-01-21 04:58:01 -0500

    fix location

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,112 +1,142 @@
 <template>
-  <div class="hello">
+  <div class="container">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+    <GmapAutocomplete
+      class="form-control"
+      placeholder="Escriba su dirección"
+      id="address"
+      ref="address"
+      @place_changed="getAddressData"
+      v-on:error="handleError"
+      :options="{
+        componentRestrictions: { country: 'pe' },
+        bounds: defaultBounds,
+        strictBounds: true,
+        fields: ['geometry', 'formatted_address']
+      }"
+    >
+    </GmapAutocomplete>
+    <div class="m-3"><b>Dirección:</b> {{ address }}</div>
+    <GmapMap
+      style="width: 100%; height: 300px"
+      :center="locationDetected"
+      :zoom="18"
+      :options="{
+        disableDefaultUI: true,
+        fullscreenControl: false,
+        mapTypeControl: false,
+        rotateControl: false,
+        scaleControl: false,
+        streetViewControl: false,
+        zoomControl: false
+      }"
+      @center_changed="resetCenterChange"
+    >
+      <GmapMarker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="locationDetected"
+        :clickable="false"
+        :draggable="false"
+        :icon="{
+          url: require('../assets/Map-Marker.png')
+        }"
+      />
+    </GmapMap>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "HelloWorld",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      locationDetected: {},
+      markers: [
+        {
+          position: {
+            lat: "",
+            lng: ""
+          }
+        }
+      ],
+      address: "",
+      mapsKey: "AIzaSyCqupodOy4ShkVk0SzzoRQDuRaQn3IHIcY",
+      defaultBounds: {
+        northeast: { lat: -5.139501860821229, lng: -80.61527252062896 },
+        southwest: { lat: -5.246669630661016, lng: -80.71834006713638 }
+      }
+    };
+  },
+  created() {
+    this.getLocation();
+  },
+  watch: {},
+  methods: {
+    async resetCenterChange(evnt) {
+      this.locationDetected = {
+        lat: evnt.lat(),
+        lng: evnt.lng()
+      };
+      await axios
+        .get(
+          `https://maps.googleapis.com/maps/api/geocode/json?latlng=${evnt.lat()},${evnt.lng()}&key=${
+            this.mapsKey
+          }`
+        )
+        .then(
+          response => (
+            (this.address = response.data.results[0].formatted_address),
+            (this.$refs.address.$refs.input.value =
+              response.data.results[0].formatted_address)
+          )
+        );
+    },
+    getAddressData(addressData) {
+      this.address = addressData.formatted_address;
+      this.locationDetected.lat = addressData.geometry.location.lat();
+      this.locationDetected.lng = addressData.geometry.location.lng();
+    },
+    handleError(error) {
+      alert(error);
+    },
+    getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(
+          position => {
+            this.locationDetected = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            this.markers[0].position = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+            axios
+              .get(
+                `https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.coords.latitude},${position.coords.longitude}&key=${this.mapsKey}`
+              )
+              .then(
+                response => (
+                  (this.address = response.data.results[0].formatted_address),
+                  (this.$refs.address.$refs.input.value =
+                    response.data.results[0].formatted_address)
+                )
+              );
+          },
+          error => {
+            console.log(error.message);
+          }
+        );
+      } else {
+        console.log("Navegador no soportado");
+      }
+    }
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,24 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { BootstrapVue } from "bootstrap-vue";
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import * as VueGoogleMaps from "gmap-vue";
 
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: "AIzaSyCqupodOy4ShkVk0SzzoRQDuRaQn3IHIcY",
+    libraries: "places",
+    region: "PE",
+    language: "es"
+  },
+  installComponents: true
+});
 
 new Vue({
   router,
